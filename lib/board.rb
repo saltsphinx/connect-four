a1a7e6487cf5b199f2776b36b150f66f2390930d
saltsphinx <tremar.thomas19@gmail.com>
2021-11-17 11:18:06 -0600

    project complete

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,38 @@
+require_relative './disc'
+
 class Board
-  attr_accesor :board
+  attr_accessor :columns, :winner
   def initialize
-    @board = [[], [], [], [], [], [], []]
+    @columns = [[], [], [], [], [], [], []]
   end
 
-  def insert(column, disc)
-    board[column] << disc
-    
+  def insert(column, color)
+    disc = Disc.new(color, self)
+    columns[column] << disc
+    disc.disc_cycle(column, columns[column].size - 1)
+
+    if disc.directions.map { |_, direction| direction.length }.any? { |size| size >= 4 }
+      @winner = disc.color
+    end
+  end
+
+  def game_end?
+    @winner.nil? ? false : true
+  end
+
+  def display_board
+    [*0..5].reverse.each do |index|
+      columns.each_with_index do |column, c_index|
+        disc = column[index]
+
+        print "|" if c_index == 0
+        if disc.nil?
+          print " \u26ab |".encode('utf-8')
+        else
+          disc.color == 'Yellow' ? print(" \u26a1 |".encode('utf-8')) : print(" \u26d4 |".encode('utf-8'))
+        end
+        puts "\n" if c_index == 6
+      end
+    end
   end
 end
--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -1,12 +1,16 @@
+require_relative './board'
+
 class ConnectFour
   def initialize
     @players = ['Red', 'Yellow']
     @current_player = @players.first
-    @board
+    @board = Board.new
   end
 
   def play
     main_menu
+    game_cycle
+    game_ended
   end
 
   def user_input(min, max)
@@ -23,8 +27,26 @@ class ConnectFour
     info if option_select == 2
   end
 
-  def player_turn
+  def game_cycle
+    until @board.game_end?
+      @board.display_board
+      player_turn
+      rotate_player
+      change_player
+    end
+  end
 
+  def player_turn
+    input = nil
+    while input.nil?
+      result = user_input(1, 7)
+      if @board.columns[result - 1].size < 6
+        input = result
+      else
+        puts "Column #{result} is full!"
+      end
+    end
+    @board.insert(input - 1, @current_player)
   end
   
   def rotate_player
@@ -38,6 +60,12 @@ class ConnectFour
   def splash
     splash_text = "Connect Four\n\nOptions:\n1 - Play\n2 - Info"
     puts splash_text
+  end
+
+  def game_ended
+    @board.display_board
+    puts 'Game over!'
+    puts "#{@board.winner} wins!"
   end
 
   def info
